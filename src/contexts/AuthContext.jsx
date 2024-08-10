7f07e95deaf8e046bfed867c0177f54195022410
kryptyko <kryptyko@gmail.com>
2024-08-10 13:20:56 -0300

    se agrego boton login logout por contexto

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -16,6 +16,7 @@ function reducer(state, action) {
         case ACTIONS.LOGIN:
             return {
                 ...state,
+                user__id: action.payload.user__id,
                 token: action.payload,
                 isAuthenticated: true,
             };
@@ -30,6 +31,7 @@ function reducer(state, action) {
 
 function AuthProvider({ children }) {
     const [state, dispatch] = useReducer(reducer, {
+        user__id: localStorage.getItem("user__id"),
         token: localStorage.getItem("authToken"),
         isAuthenticated: localStorage.getItem("authToken") ? true : false,
     });
@@ -37,15 +39,20 @@ function AuthProvider({ children }) {
     const location = useLocation();
 
     const actions = {
-        login: (token) => {
-            dispatch({ type: ACTIONS.LOGIN, payload: token });
+        login: (token, user__id) => {
+            dispatch({ 
+                type: ACTIONS.LOGIN, 
+                payload: {token, user__id },
+        });
             localStorage.setItem("authToken", token);
+            localStorage.setItem("user__id", user__id);
             const origin = location.state?.from?.pathname || "/";
             navigate(origin);
         },
         logout: () => {
             dispatch({ type: ACTIONS.LOGOUT });
             localStorage.removeItem("authToken");
+            localStorage.removeItem("user__id");
         },
     };
 
