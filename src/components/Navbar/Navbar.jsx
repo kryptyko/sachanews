64ad4a7068eb0969f2d14d72869711f076a34989
krypto <kryptyko@gmail.com>
2024-08-10 20:56:28 -0300

    modificaciones al navbar nuevanotcia con usuario logueado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,9 @@ import { useState, useEffect } from "react";
 import { useAuth } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-
 export default function Navbar() {
     const [categories, setCategories] = useState([]);
     const { isAuthenticated} = useAuth("state");
-    console.log(isAuthenticated);
     const { logout } = useAuth("actions");
     const navigate = useNavigate();
 
@@ -24,53 +22,45 @@ export default function Navbar() {
         fetchCategories();
     }, []);
 
-    //intento de poner el boton condicional
-    // const handleLoginLogout = () => {
-    //     if (isAuthenticated) {
-    //         logout();
-    //         navigate("/login"); // Redirige al usuario al iniciar sesión
-    //     } else {
-    //         navigate("/login");
-    //     }
-    // };
     return (
-      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
-              <a className="navbar-item has-text-warning" href="/">Inicio</a>
-              <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-              </a>
+            <a className="navbar-item has-text-warning" href="/">Inicio</a>
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
-
+      
           <div className="navbar-menu">
-              <div className="navbar-start">
-                  {categories.map(category => (
-                      <a key={category.id} className="navbar-item has-text-white" href={`/category/${category.id}`}>
-                          {category.name}
-                      </a>
-                  ))}
-              </div>
-              <div className="navbar-end">
-                  <div className="navbar-item">
-                      <button className="button is-warning" onClick={
-                        isAuthenticated
-                            ? () => {
-                                  logout();
-                                  window.alert('¡Se ha cerrado secion correctamente!');
-                                  
-                              }
-                            : () => {
-                                  navigate("/login");
-                              }
+            <div className="navbar-start">
+              {categories.map(category => (
+                <a key={category.id} className="navbar-item has-text-white" href={`/category/${category.id}`}>
+                  {category.name}
+                </a>
+              ))}
+            </div>
+            <div className="navbar-end">
+              {isAuthenticated && (
+                <a className="navbar-item has-text-white" href="/articles/add">
+                  Nueva Noticia
+                </a>
+              )}
+              <div className="navbar-item">
+                <button className="button is-warning" onClick={
+                  isAuthenticated
+                    ? () => {
+                      logout();
                     }
-                >
-                    {isAuthenticated ? "Cerrar sesión" : "Iniciar Sesión"}
+                    : () => {
+                      navigate("/login");
+                    }
+                }>
+                  {isAuthenticated ? "Cerrar sesión" : "Iniciar Sesión"}
                 </button>
-                  </div>
               </div>
+            </div>
           </div>
-      </nav>
-  );
-}
+        </nav>
+      );}
