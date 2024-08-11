0cccae0839d7eb1a37d51106093a8b51364e2c2f
MarceXamena <marceloxamena@gmail.com>
2024-08-11 18:29:04 -0300

    Intentando fijar Header

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -4,15 +4,16 @@ import { ThemeProvider } from "@teishi/bulma_theme";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import { useLocation } from "react-router-dom";
+import "./Layout.css";
 
 export default function Layout() {
     const { pathname } = useLocation();
     const shouldRenderNavbar = !pathname.includes("/login");
     return (
         <AuthProvider>
-            <Banner />
+            <Banner className="fixed-banner" />
             {/* <Navbar /> */}
-            {shouldRenderNavbar && <Navbar />}
+            {shouldRenderNavbar && <Navbar className="fixed-navbar" />}
             <div
                 className={`hero is-fullheight is-flex is-flex-direction-column`}
             >
