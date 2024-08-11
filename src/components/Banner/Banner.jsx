3ebad6f661410ab65ada99803e8251dff0fca6b5
kryptyko <kryptyko@gmail.com>
2024-08-11 14:23:36 -0300

    se saco el boton de inicio de sesion del navbar y se lo agrego al banner, se agrego el link de ir al perfil desde el baner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,42 +1,56 @@
-// import './Banner.css';
-// import { Link } from 'react-router-dom';
-// const Banner = () => {
-//   return (
-//     <>
-//       <div className="has-text-centered">
-//         <Link to="/">
-//             <h1 className="title has-text-primary">
-//               SachaNEWS
-//             </h1>
-//         </Link>
-//           </div>
-        
-        
-//     </>
-//   );
-// };
-
-// export default Banner;
 
 import './Banner.css';
 import { Link } from 'react-router-dom';
 import SachaNEWSSvg from '../../assets/sachanews.svg';
 import { useAuth } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 
 const Banner = () => {
-  const { isAuthenticated } = useAuth("state");
-  //const {first_name} =useAuth("state");
-//  console.log( first_name)
-  console.log( isAuthenticated)
+  const { isAuthenticated, first_name } = useAuth("state");
+  const { logout } = useAuth("actions");
+  const navigate = useNavigate();
+
+  console.log( first_name)
+
   return (
     <>
       <div className="has-text-centered">
-        <Link to="/">
-          <div className="title has-text-primary">
-            <img src={SachaNEWSSvg} alt="SachaNEWS Logo" className="logo-image" />
-            <div> {isAuthenticated}</div>
+        <div className="columns is-vcentered">
+          <div className="column"></div>
+          <div className="column">
+            <div className="title has-text-primary d-flex justify-content-center align-items-center">
+              <img src={SachaNEWSSvg} alt="SachaNEWS Logo" className="logo-image" />
+            </div>
           </div>
-        </Link>
+          <div className="column">
+
+            {isAuthenticated && (
+              <div className="d-flex justify-content-end">
+                <div className="user-greeting mr-3">
+              Hola{' '}
+              <Link to="/profile" className="has-text-primary">
+                <span>{first_name}</span>
+              </Link>
+            </div>
+              </div>
+            )}
+            <button
+                  className="button is-warning is-small is-outlined"
+                  onClick={() => {
+                    isAuthenticated ? logout() : navigate('/login');
+                    window.alert(isAuthenticated ? 'Sesión cerrada correctamente' : '');
+                  }}
+                >
+                  {isAuthenticated ? 'Cerrar sesión' : 'Iniciar Sesión'}
+                </button>
+          </div>
+        </div>
+        {isAuthenticated && (
+          <div className="d-flex justify-content-end">
+            
+          </div>
+        )}
       </div>
     </>
   );
