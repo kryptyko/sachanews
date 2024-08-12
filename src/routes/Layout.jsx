bb09041bc4cc594263e60871768260ee9751259c
MarceXamena <marceloxamena@gmail.com>
2024-08-11 23:22:17 -0300

    ocultar navbar en login

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
 import { ThemeProvider } from "@teishi/bulma_theme";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
 export default function Layout() {
+    const { pathname } = useLocation();
+    const shouldRenderNavbar = !["/login"].includes(pathname);
     return (
         <AuthProvider>
             <Banner />
-            <Navbar />
+            {shouldRenderNavbar && <Navbar />}
             <div
                 className={`hero is-fullheight is-flex is-flex-direction-column`}
             >
